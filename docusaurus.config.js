7e7745779787c7441addbb3e77170bb62b02ef87
Simon <email@simonvictory.com>
2020-07-21 15:34:26 +1000

    analytics moved, font

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,12 @@ module.exports = {
           position: "right",
         },
       ],
+    },    
+    googleAnalytics: {
+      trackingID: 'UA-11311405-8'
+    },
+    gtag: {
+      trackingID: 'UA-11311405-8',
     },
     announcementBar: {
       id: "support_us", // Any value that will identify this message.
@@ -61,12 +67,6 @@ module.exports = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
-        googleAnalytics: {
-          trackingID: 'UA-11311405-8'
-        },
-        gtag: {
-          trackingID: 'UA-11311405-8',
-        }
       },
     ],
   ],
