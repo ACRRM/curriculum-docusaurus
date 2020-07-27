e16dffda4c3db2166c362230584a5ca1e44a4972
Simon <email@simonvictory.com>
2020-07-27 13:10:55 +1000

    add PWA

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,12 +22,12 @@ module.exports = {
           position: "right",
         },
       ],
-    },    
+    },
     googleAnalytics: {
-      trackingID: 'UA-11311405-8'
+      trackingID: "UA-11311405-8",
     },
     gtag: {
-      trackingID: 'UA-11311405-8',
+      trackingID: "UA-11311405-8",
     },
     announcementBar: {
       id: "support_us", // Any value that will identify this message.
@@ -48,9 +48,64 @@ module.exports = {
   },
   plugins: [
     // path.resolve(__dirname, "./node_modules/docusaurus-lunr-search/"),
-    "docusaurus-plugin-sass",
+    // "docusaurus-plugin-auto-sidebars"
+    // "docusaurus-plugin-sass",
+    [
+      "@docusaurus/plugin-pwa",
+      {
+        debug: true,
+        offlineModeActivationStrategies: ["appInstalled", "queryString"],
+        pwaHead: [
+          {
+            tagName: "link",
+            rel: "icon",
+            href: "/img/icons/icon-512x512.png",
+          },
+          {
+            tagName: "link",
+            rel: "manifest",
+            href: "/manifest.json",
+          },
+          {
+            tagName: "meta",
+            name: "theme-color",
+            content: "#e87500",
+          },
+          {
+            tagName: 'meta',
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes',
+          },
+          {
+            tagName: 'meta',
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: '#000',
+          },
+          {
+            tagName: 'link',
+            rel: 'apple-touch-icon',
+            href: '/img/icons/icon-512x512.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'mask-icon',
+            href: '/img/ACRRM_logo_minimal_v2.svg',
+            color: '#e87500',
+          },
+          {
+            tagName: 'meta',
+            name: 'msapplication-TileImage',
+            content: '/img/icons/icon-512x512.png',
+          },
+          {
+            tagName: 'meta',
+            name: 'msapplication-TileColor',
+            content: '#000',
+          },
+        ],
+      },
+    ],
   ],
-  // plugins: ['docusaurus-plugin-auto-sidebars'],
   presets: [
     [
       "@docusaurus/preset-classic",
@@ -60,7 +115,7 @@ module.exports = {
           showLastUpdateTime: true,
           // It is recommended to set document id as docs home page (`docs/` path).
           routeBasePath: "/",
-          sidebarPath: require.resolve("./sidebars.auto.js")
+          sidebarPath: require.resolve("./sidebars.auto.js"),
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
