6896362b44d1ba23fd65b5a3c53b8ecd1f7c237f
Gav <g.long@acrrm.org.au>
2021-12-17 12:12:22 +1000

    tinkering with re-adding rg curriculum

--- sidebars.auto.js
+++ sidebars.auto.js
@@ -1,82 +1,81 @@
 module.exports = {
-  firstSidebar: [
-    "_index",
-    {
-      type: "ref",
-      id: "fellowship-assessment/introduction",
-    },
-    {
-      type: "ref",
-      id: "fellowship-training/introduction",
-    },
-    {
-      type: "ref",
-      id: "supervisors-and-training-posts/introduction",
-    },
-    {
-      type: "ref",
-      id: "training-organisations/introduction",
-    },
-  ],  
-  "Fellowship Assessment": [
-    "fellowship-assessment/introduction"
-  ],
-  "Fellowship Training": [
-    "fellowship-training/introduction"
-  ],
-  "Supervisors and Training Posts": [
-    "supervisors-and-training-posts/introduction"
-  ],
-  "Training Organisations": [
-    "training-organisations/introduction"
-  ]
+    firstSidebar: [
+        "_index",
+        {
+            type: "ref",
+            id: "fellowship-assessment/introduction",
+        },
+        {
+            type: "ref",
+            id: "fellowship-training/introduction",
+        },
+        {
+            type: "ref",
+            id: "supervisors-and-training-posts/introduction",
+        },
+        {
+            type: "ref",
+            id: "training-organisations/introduction",
+        },
+    ],
+    "Fellowship Assessment": [
+        "fellowship-assessment/introduction"
+    ],
+    "Fellowship Training": [
+        "fellowship-training/introduction"
+    ],
+    "Supervisors and Training Posts": [
+        "supervisors-and-training-posts/introduction"
+    ],
+    "Training Organisations": [
+        "training-organisations/introduction"
+    ],
+    "Rural Generalist": [
+        "rural-generalist/introduction",
+        "rural-generalist/curriculum",
+        "rural-generalist/competency-domains",
+        {
+            type: "category",
+            label: "All Learning Areas",
+            items: [
+                "rural-generalist/learning-areas/aboriginal-and-torres-strait-islander-health",
+                "rural-generalist/learning-areas/academic-practice",
+                "rural-generalist/learning-areas/addictive-behaviours",
+                "rural-generalist/learning-areas/adult-internal-medicine",
+                "rural-generalist/learning-areas/aged-care",
+                "rural-generalist/learning-areas/anaesthetics",
+                "rural-generalist/learning-areas/chronic-disease",
+                "rural-generalist/learning-areas/collaborator",
+                "rural-generalist/learning-areas/communicator",
+                "rural-generalist/learning-areas/dermatology",
+                "rural-generalist/learning-areas/diagnostic-imaging",
+                "rural-generalist/learning-areas/differential-diagnosis",
+                "rural-generalist/learning-areas/digital-health",
+                "rural-generalist/learning-areas/emergency",
+                "rural-generalist/learning-areas/genetics",
+                "rural-generalist/learning-areas/health-advocate",
+                "rural-generalist/learning-areas/history-taking",
+                "rural-generalist/learning-areas/investigations",
+                "rural-generalist/learning-areas/leader",
+                "rural-generalist/learning-areas/mental-health",
+                "rural-generalist/learning-areas/musculoskeletal",
+                "rural-generalist/learning-areas/obstetrics-and-gynaecology",
+                "rural-generalist/learning-areas/occupational-health",
+                "rural-generalist/learning-areas/ophthalmology",
+                "rural-generalist/learning-areas/oral-health",
+                "rural-generalist/learning-areas/paediatrics",
+                "rural-generalist/learning-areas/palliative-care",
+                "rural-generalist/learning-areas/pharmaceuticals",
+                "rural-generalist/learning-areas/physical-examination",
+                "rural-generalist/learning-areas/population-health",
+                "rural-generalist/learning-areas/procedural-skills",
+                "rural-generalist/learning-areas/professional",
+                "rural-generalist/learning-areas/rehabilitation",
+                "rural-generalist/learning-areas/remote-medicine",
+                "rural-generalist/learning-areas/scholar",
+                "rural-generalist/learning-areas/sexual-health",
+                "rural-generalist/learning-areas/surgery",
+            ],
+        },
+    ],
 };
-
-// "Rural Generalist": [
-//   "rural-generalist/introduction",
-//   "rural-generalist/curriculum",
-//   "rural-generalist/competency-domains",
-//   {
-//     type: "category",
-//     label: "All Learning Areas",
-//     items: [
-//       "rural-generalist/learning-areas/aboriginal-and-torres-strait-islander-health",
-//       "rural-generalist/learning-areas/academic-practice",
-//       "rural-generalist/learning-areas/addictive-behaviours",
-//       "rural-generalist/learning-areas/adult-internal-medicine",
-//       "rural-generalist/learning-areas/aged-care",
-//       "rural-generalist/learning-areas/anaesthetics",
-//       "rural-generalist/learning-areas/chronic-disease",
-//       "rural-generalist/learning-areas/collaborator",
-//       "rural-generalist/learning-areas/communicator",
-//       "rural-generalist/learning-areas/dermatology",
-//       "rural-generalist/learning-areas/diagnostic-imaging",
-//       "rural-generalist/learning-areas/differential-diagnosis",
-//       "rural-generalist/learning-areas/digital-health",
-//       "rural-generalist/learning-areas/emergency",
-//       "rural-generalist/learning-areas/genetics",
-//       "rural-generalist/learning-areas/health-advocate",
-//       "rural-generalist/learning-areas/history-taking",
-//       "rural-generalist/learning-areas/investigations",
-//       "rural-generalist/learning-areas/leader",
-//       "rural-generalist/learning-areas/mental-health",
-//       "rural-generalist/learning-areas/musculoskeletal",
-//       "rural-generalist/learning-areas/obstetrics-and-gynaecology",
-//       "rural-generalist/learning-areas/occupational-health",
-//       "rural-generalist/learning-areas/ophthalmology",
-//       "rural-generalist/learning-areas/oral-health",
-//       "rural-generalist/learning-areas/paediatrics",
-//       "rural-generalist/learning-areas/palliative-care",
-//       "rural-generalist/learning-areas/pharmaceuticals",
-//       "rural-generalist/learning-areas/physical-examination",
-//       "rural-generalist/learning-areas/population-health",
-//       "rural-generalist/learning-areas/procedural-skills",
-//       "rural-generalist/learning-areas/professional",
-//       "rural-generalist/learning-areas/rehabilitation",
-//       "rural-generalist/learning-areas/remote-medicine",
-//       "rural-generalist/learning-areas/scholar",
-//       "rural-generalist/learning-areas/sexual-health",
-//       "rural-generalist/learning-areas/surgery",
-//     ],
-//   },
-// ],
